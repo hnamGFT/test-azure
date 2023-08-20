5360c9774e6dbfe26238898226912b9bd315b23b
Muhammad Awab <98603853+Muhammad-Awab@users.noreply.github.com>
2023-08-20 14:56:40 -0400

    Update providers.tf

--- azure/sample-resource/providers.tf
+++ azure/sample-resource/providers.tf
@@ -9,8 +9,8 @@ terraform {
 }
 provider "azurerm" {
   features {}
-  client_id       = var.AZURE_AD_CLIENT_ID
-  subscription_id = var.AZURE_SUBSCRIPTION_ID
-  tenant_id       = var.AZURE_AD_TENANT_ID
+  client_id       = "65d0fe7c-852c-4775-836e-741243a07b6d"
+  subscription_id = "f6fd0be5-b86d-43cb-a458-b589cae1c030"
+  tenant_id       = "91a30fb8-9714-494d-946a-8b9a6547c92a"
 }
 //
