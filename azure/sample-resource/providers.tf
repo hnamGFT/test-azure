8b7ad97f960a832125051a57d4a1fafa2d15f86e
Muhammad Awab <98603853+Muhammad-Awab@users.noreply.github.com>
2023-08-20 15:00:21 -0400

    Update providers.tf

--- azure/sample-resource/providers.tf
+++ azure/sample-resource/providers.tf
@@ -8,6 +8,7 @@ terraform {
   }
 }
 provider "azurerm" {
+  use_oidc = true
   features {}
   client_id       = "65d0fe7c-852c-4775-836e-741243a07b6d"
   subscription_id = "f6fd0be5-b86d-43cb-a458-b589cae1c030"
